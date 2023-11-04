95a774e2309e439c2dd433d094f5a3722eb08e77
Benjamin Ocampo <nicolasbenjaminocampo@gmail.com>
2023-11-04 17:29:33 +0100

    Add makefile, requirements, and environment config

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="implicit_hate_generator",
+      description="Playing the Part of the Sharp Bully: Generating Adversarial Examples for Implicit Hate Speech Detection",
+      author="Nicolás Benjamín Ocampo",
+      author_email="nicolas-benjamin.ocampo@unice.fr",
+      version="1.0",
+      license='BSL',
+      packages=find_packages())
